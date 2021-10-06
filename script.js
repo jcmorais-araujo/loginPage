c9c497fb7f9e636744dbf2b2ce146db25184b51f
Joao Carlos Morais <jcmorais.araujo@gmail.com>
2021-10-06 13:23:20 -0300

    adicao da logica de programacao

--- script.js
+++ script.js
@@ -8,4 +8,21 @@ senha.addEventListener('input', () => {
     } else {
         mensagem.style.display = "none";
     }
+
+    if (senha.value.length < 4) {
+        aviso.innerHTML = "fraca";
+        senha.style.borderColor = "#ff5925";
+        mensagem.style.color = "#ff5925";
+    } 
+    else if (senha.value.length >= 4 && senha.value.length < 6) {
+        aviso.innerHTML = "moderada";
+        senha.style.borderColor = "yellow";
+        mensagem.style.color = "yellow";
+    } 
+    else if (senha.value.length > 6) {
+        aviso.innerHTML = "forte";
+        senha.style.borderColor = "#26d730";
+        mensagem.style.color = "#26d730";
+    }
+
 })
